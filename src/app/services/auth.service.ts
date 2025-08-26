c4a6b82475b12979ae2c0081cd2fc841fa68a018
Sujith Patil <emailpatilsujith19@gmail.com>
2025-08-26 13:05:33 +0530

    login check

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,26 +2,13 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(private router: Router) {
-    
-  }
+  constructor(private router: Router) {}
 
   isAuthenticated() {
-      const cookie:string = document.cookie;
-      const cookiesList = cookie.split(';');
-      let userLoggedIn = false;
-      
-      cookiesList.filter( cookie => {
-        if(cookie.indexOf('session-id') > -1) {
-          userLoggedIn = true;
-          return;
-        }
-      } )
-      return userLoggedIn;
+    const userLoggedIn = !!localStorage.getItem('session-id');
+    return userLoggedIn;
   }
-
 }
